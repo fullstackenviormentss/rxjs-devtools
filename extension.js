e9d0c0386104bdc1974675c08d7abd068934de68
Adrian Faciu <adrian.faciu@visma.com>
2017-11-03 16:51:26 +0200

    Panel is now visible

--- extension.js
+++ extension.js
@@ -1,1 +1,13 @@
-console.log('Extensions running...')
+chrome.devtools.panels.create("RxJs",
+    "MyPanelIcon.png",
+    "panel.html",
+    function (panel) {
+        console.log('Extension running...');
+    }
+);
+
+// chrome.devtools.panels.elements.createSidebarPane("Sidebar",
+//     function (sidebar) {
+//         sidebar.setPage("sidebar.html");
+//     }
+// );
